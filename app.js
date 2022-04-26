c37563dd253e550a210591ad7b056c861b41ad78
Colter Garrison <102547626+Colter-Garrison@users.noreply.github.com>
2022-04-25 17:01:02 -0700

    styling first draft

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // import functions and grab DOM elements
 const button1 = document.getElementById('shell-1-button');
+const button2 = document.getElementById('shell-2-button');
+const button3 = document.getElementById('shell-3-button');
 // console.log(button1);
 
 const shell1 = document.getElementById('shell-1');
@@ -14,5 +16,9 @@ let wins = 0;
 let losses = 0;
 
 button1.addEventListener('click', () => {
-  
+  shell1.classList.remove('reveal');
+  shell2.classList.remove('reveal');
+  shell3.classList.remove('reveal');
+
+  console.log('clicking button 1')
 })
